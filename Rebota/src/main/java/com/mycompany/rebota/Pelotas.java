38a146d469ece44f7d552df2a4497e1f2402b057
regaaron <aaron_lopez222@hotmail.com>
2023-03-26 23:36:15 -0600

    git diff

--- Rebota/src/main/java/com/mycompany/rebota/Pelotas.java
+++ Rebota/src/main/java/com/mycompany/rebota/Pelotas.java
@@ -11,121 +11,83 @@ import java.awt.Color;
  * @author aaron
  */
 public class Pelotas {
-    final static int ANCHO =1000;
+
+    final static int ANCHO = 1000;
     final static int ALTO = 600;
-    final static int DIAMETRO= 20;
-    float x,y;
-    float vx,vy;
+    final static int DIAMETRO = 20;
+    float x, y;
+    float vx, vy;
     static int c1;
-     static int c2;
-    Color color ;
-    Pelotas(){
-        x=ANCHO/2;//(int)(Math.random()*500+1);
-        y=(int)(Math.random()*350+1);
-        vx=(int)(Math.random()*(300-150+1)+150);
-        vy=(int)(Math.random()*(400-150+1)+150);
-        color= new Color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
-        c1=c2=0;
+    static int c2;
+    Color color;
+
+    Pelotas() {
+        x = ANCHO / 2;//(int)(Math.random()*500+1);
+        y = (int) (Math.random() * 350 + 1);
+        vx = (int) (Math.random() * (300 - 150 + 1) + 150);
+        vy = (int) (Math.random() * (400 - 150 + 1) + 150);
+        color = new Color((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256));
+        c1 = c2 = 0;
 
     }
-    private float[] rotate(float vx, float vy, float sin, float cos) {
-    float[] result = new float[2];
-    result[0] = cos * vx - sin * vy;
-    result[1] = sin * vx + cos * vy;
-    return result;
-}
 
-private float[] elasticCollision(float[] v1, float[] v2) {
-    float[] result = new float[4];
-    result[0] = v2[0];
-    result[1] = v1[1];
-    result[2] = v1[0];
-    result[3] = v2[1];
-    return result;
-}
-    
-    public void Fisica(float dt, Pelotas []p,Barra b1,Barra b2){
-        
-        this.x += this.vx*dt;
-          y +=vy*dt;
-    
-        if(vx<0 && x<=0 ){
-            x=ANCHO/2;//(int)(Math.random()*500+1);
-            y=(int)(Math.random()*ALTO+1);
-            vx=-vx;
+   
+
+    public void Fisica(float dt, Pelotas[] p, Barra b1, Barra b2) {
+
+        this.x += this.vx * dt;
+        y += vy * dt;
+
+        if (vx < 0 && x <= 0) {
+            x = ANCHO / 2;//(int)(Math.random()*500+1);
+            y = (int) (Math.random() * ALTO + 1);
+            vx = -vx;
             c1++;
-            System.out.println("c1="+c1);
+            System.out.println("c1=" + c1);
         }
-           
-        
-        if(vx>0 && x+ DIAMETRO >=ANCHO){
-             x=ANCHO/2;//(int)(Math.random()*500+1);
-            y=(int)(Math.random()*ALTO+1);
-            vx=-vx;
+
+        if (vx > 0 && x + DIAMETRO >= ANCHO) {
+            x = ANCHO / 2;//(int)(Math.random()*500+1);
+            y = (int) (Math.random() * ALTO + 1);
+            vx = -vx;
             c2++;
-            System.out.println("c2="+c2);
+            System.out.println("c2=" + c2);
 
         }
-        
-        
-        if(x+DIAMETRO>b1.x&&x+DIAMETRO<b1.x+b1.ANCHO && y+DIAMETRO>b1.y&&y<b1.y+b1.ALTO){
-            vx=-vx;
-            vy=-vy;
-            //System.out.println(x+", "+y+","+xbarra+","+ybarra);
-        }
-        
-         if(x>b2.x&&x<b2.x+b2.ANCHO && y>b2.y&&y<b2.y+b2.ALTO){
-            vx=-vx;
-            vy=-vy;
-            //System.out.println(x+", "+y+","+xbarra+","+ybarra);
-        }
-            
-        
-        
-         if(vy<0 && y<=0 || vy>0 && y+DIAMETRO >= ALTO)
-            vy=-vy;
-          for (int i = 0; i < p.length; i++) {
-              
-        if (this != p[i]) {
-            float dx = x - p[i].x;
-            float dy = y - p[i].y;
-            float distance = (float) Math.sqrt(dx * dx + dy * dy);
-            
-            if (distance <=DIAMETRO) {
-                
-                vx=-vx;
-                vy=-vy;
-                p[i].vx=-vx;
-                p[i].vy=-vy;
-                color= new Color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
-                p[i].color= new Color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
 
-                 /*
-                // Las pelotas chocan, cambiamos su dirección
-                float angle = (float) Math.atan2(dy, dx);
-                float sin = (float) Math.sin(angle);
-                float cos = (float) Math.cos(angle);
-                
-                // Rotamos la velocidad de ambas pelotas
-                float[] v1 = rotate(vx, vy, sin, cos);
-                float[] v2 = rotate(p[i].vx, p[i].vy, sin, cos);
-                
-                // Calculamos las nuevas velocidades
-                float[] finalVelocities = elasticCollision(v1, v2);
-                
-                // Rotamos las velocidades de vuelta
-                float[] v1F = rotate(finalVelocities[0], finalVelocities[1], cos, -sin);
-                float[] v2F = rotate(finalVelocities[2], finalVelocities[3], cos, -sin);
-                
-                // Asignamos las nuevas velocidades a las pelotas
-                vx = v1F[0];
-                vy = v1F[1];
-                p[i].vx = v2F[0];
-                p[i].vy = v2F[1];
-                */
+        if (x + DIAMETRO > b1.x && x + DIAMETRO < b1.x + b1.ANCHO && y + DIAMETRO > b1.y && y < b1.y + b1.ALTO) {
+            vx = -vx;
+            vy = -vy;
+            //System.out.println(x+", "+y+","+xbarra+","+ybarra);
+        }
+
+        if (x > b2.x && x < b2.x + b2.ANCHO && y > b2.y && y < b2.y + b2.ALTO) {
+            vx = -vx;
+            vy = -vy;
+            //System.out.println(x+", "+y+","+xbarra+","+ybarra);
+        }
+
+        if (vy < 0 && y <= 0 || vy > 0 && y + DIAMETRO >= ALTO) {
+            vy = -vy;
+        }
+        for (int i = 0; i < p.length; i++) {
+
+            if (this != p[i]) {
+                float dx = x - p[i].x;
+                float dy = y - p[i].y;
+                float distance = (float) Math.sqrt(dx * dx + dy * dy);
+
+                if (distance <= DIAMETRO) {
+                   vx=-vx;
+                   vy=-vy;
+                   p[i].vx=-vx;
+                   p[i].vy=-vy;
+                    color = new Color((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256));
+                    p[i].color = new Color((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256));
+
+                }
             }
         }
     }
-    }
-    
+
 }
--- Rebota/src/main/java/com/mycompany/rebota/Rebota.java
+++ Rebota/src/main/java/com/mycompany/rebota/Rebota.java
@@ -10,6 +10,13 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.net.URL;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
 
 public class Rebota extends JComponent {
@@ -19,10 +26,11 @@ public class Rebota extends JComponent {
     private final static int DIAMETRO = 20;
     private Timer timer;
     private boolean arriba, abajo, w, s;
-    Pelotas[] p = new Pelotas[3];
+    Pelotas[] p = new Pelotas[5];
     Barra b1 = new Barra(ANCHO - 100);
     Barra b2 = new Barra(50);
     JLabel puntos1, puntos2;
+    
 
     public void cicloPrincipalJuego() throws Exception {
         long tiempoViego = System.nanoTime();
@@ -43,13 +51,14 @@ public class Rebota extends JComponent {
     public void paint(Graphics g) {
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, ANCHO, ALTO);
+        BasicStroke dashedStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, new float[]{5.0f, 2.0f}, 0.0f);
+
         /* g.setColor(new Color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256)));
    g.setColor(Color.RED); 
    g.fillOval(Math.round(x), Math.round(y), DIAMETRO, DIAMETRO);
     g.setColor(Color.BLACK);
     g.drawString("hola",(int) x+20,(int) y +20);
          */
-
         Font fuente = new Font("URW Gothic", Font.PLAIN, 28);
         for (int i = 0; i < p.length; i++) {
             g.setColor(p[i].color);
@@ -57,25 +66,29 @@ public class Rebota extends JComponent {
             //System.out.println("x="+xbarra+"y="+ybarra);
 
         }
-        g.setColor(Color.WHITE);
+        g.setColor(new Color(255, 0, 255));
         g.fillRect(b1.x, b1.y, b1.ANCHO, b1.ALTO);
         g.fillRect(b2.x, b2.y, b2.ANCHO, b2.ALTO);
+        g.setColor(Color.WHITE);
         g.setFont(fuente);
         g.drawString(p[0].c2 + "", 250, 50);
         g.drawString(p[0].c1 + "", 750, 50);
 
-      
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.WHITE);
+        g2d.setStroke(new BasicStroke(5, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND, 0, new float[]{9}, 0));
+        g2d.drawLine(ANCHO / 2, 0, ANCHO / 2, ALTO);
 
     }
 
     public Rebota() {
-        
+
         setPreferredSize(new Dimension(ANCHO, ALTO));
         for (int i = 0; i < p.length; i++) {
             p[i] = new Pelotas();
             //p[i].toString();
         }
-            timer = new Timer(10, new ActionListener() {
+        timer = new Timer(10, new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 // Actualiza la posición de las barras aquí
                 if (arriba && b1.y > 0) {
@@ -93,8 +106,6 @@ public class Rebota extends JComponent {
             }
         });
         timer.start();
-        puntos1 = new JLabel("Hola");
-        puntos1 = new JLabel("Hola");
 
         addKeyListener(new KeyAdapter() {
             public void keyPressed(KeyEvent e) {
@@ -126,9 +137,9 @@ public class Rebota extends JComponent {
 
         setFocusable(true);
 
-    }
+}
 
-    public void dibuja() throws Exception {
+public void dibuja() throws Exception {
         SwingUtilities.invokeAndWait(new Runnable() {
             public void run() {
                 paintImmediately(0, 0, ANCHO, ALTO);
